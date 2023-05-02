f42fa1f98b4e54c308351eca4c72204e03225714
Brandt Milczewski <brandt@shopdevs.com>
2023-05-02 10:28:11 -0700

    feat: Swap out the auto adapter for the Vercel adapter in sveltekit. Experiment with prerender but comment out after testing.

--- src/routes/(app)/products/[slug]/+page.ts
+++ src/routes/(app)/products/[slug]/+page.ts
@@ -1,5 +1,6 @@
 import type { PageLoad } from './$types';
 import { client } from '$lib/utils/sanity';
+// export const prerender = true;
 
 export const load = (async ({ fetch, params }) => {
 	const product = await client.fetch(
@@ -15,7 +16,7 @@ export const load = (async ({ fetch, params }) => {
 
 	if (product) {
 		return {
-			product: product[0].store
+			product: product[0]
 		};
 	}
 	return {
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			// use relative URLs similar to an anchor tag <a href="/products/1"></a>
+			// do not include group layout folders in the path such as /(app)/products/1
+			// entries: [
+			// 	'/products/1972-martin-d-28',
+			// 	'/products/preston-thompson-d-ba-brazilian-rosewood-d28-natural'
+			// ]
+		}
 	},
 	preprocess: vitePreprocess()
 };
