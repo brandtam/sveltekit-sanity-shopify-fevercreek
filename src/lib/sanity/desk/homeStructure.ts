da67c61f681a1d40bbfebd45ddb59742460c7371
Brandt Milczewski <brandt@shopdevs.com>
2023-05-02 10:30:36 -0700

    fix: Change imports to type import to make typescript happy.

--- src/lib/sanity/desk/homeStructure.ts
+++ src/lib/sanity/desk/homeStructure.ts
@@ -1,9 +1,9 @@
-import {ListItemBuilder} from 'sanity/desk'
-import defineStructure from '../utils/defineStructure'
+import type { ListItemBuilder } from 'sanity/desk';
+import defineStructure from '../utils/defineStructure';
 
 export default defineStructure<ListItemBuilder>((S) =>
-  S.listItem()
-    .title('Home')
-    .schemaType('home')
-    .child(S.editor().title('Home').schemaType('home').documentId('home'))
-)
+	S.listItem()
+		.title('Home')
+		.schemaType('home')
+		.child(S.editor().title('Home').schemaType('home').documentId('home'))
+);
