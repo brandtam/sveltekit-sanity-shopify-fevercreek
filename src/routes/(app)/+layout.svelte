08b04f19f06b73e2b4e6da94b0d5313f771624b2
Brandt Milczewski <brandt@shopdevs.com>
2023-05-04 09:09:16 -0700

    feat: Add Vercel Analytics

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
 	import Header from '$lib/components/Header.svelte';
 	import '../../app.css';
 
